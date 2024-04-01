116ce52f34838b35738744c04709e58fafb5dd8b
pkc918 <xiaopan2000918@gmail.com>
2024-04-01 22:52:17 +0800

    feat: added dto for SignIn

--- dto/account.go
+++ dto/account.go
@@ -2,6 +2,12 @@ package dto
 
 type SignUpDTO struct {
 	Email    string `json:"email" form:"email" validate:"required,email" validateMsg:"the email is required a legal email"`
-	Password string `json:"password" form:"password" validate:"required,min=8" validateMsg:"the password minimum 8 characters required"`
+	Password string `json:"password" form:"password" validate:"required,md5" validateMsg:"the password is required a legal MD5 hash"`
 	Avatar   string `json:"avatar" form:"avatar" validate:"required" validateMsg:"avatar must be a legal url"`
+}
+
+type SignInDTO struct {
+	Email    string `json:"email" form:"email" validate:"required,email" validateMsg:"the email is required a legal email"`
+	Password string `json:"password" form:"password" validate:"required,md5" validateMsg:"the password is required a legal MD5 hash"`
+	Captcha  string `json:"captcha" form:"captcha" validate:"required,min=6" validateMsg:"the captcha is minimum 6 characters required"`
 }
