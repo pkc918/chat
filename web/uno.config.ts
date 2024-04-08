a647125ea2e7e26e36b8d2578acb19c7ec71af97
pkc918 <xiaopan2000918@gmail.com>
2024-04-08 22:50:32 +0800

    feat: install icons by unocss

--- web/uno.config.ts
+++ web/uno.config.ts
@@ -1,6 +1,7 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig,presetIcons } from 'unocss'
 
 export default defineConfig({
     // ...UnoCSS options
+    presets: [presetIcons({})]
 })
