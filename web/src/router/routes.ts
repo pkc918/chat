99d10da703d8384d4b42e4e405d9075826818e3f
pkc918 <xiaopan2000918@gmail.com>
2024-04-10 23:26:59 +0800

    feat: added to initial all page router

--- web/src/router/routes.ts
+++ web/src/router/routes.ts
@@ -4,4 +4,32 @@ export const routes: RouteRecordRaw[] = [{
     path: "/chat",
     name: "Chat",
     component: () => import("@/view/Chat.vue")
+}, {
+    path: "/friend",
+    name: "Friend",
+    component: () => import("@/view/Friends.vue")
+}, {
+    path: "/collect",
+    name: "Collect",
+    component: () => import("@/view/Collect.vue")
+}, {
+    path: "/comment",
+    name: "Comment",
+    component: () => import("@/view/Comment.vue")
+}, {
+    path: "/battery",
+    name: "Battery",
+    component: () => import("@/view/Battery.vue")
+}, {
+    path: "/weather",
+    name: "Weather",
+    component: () => import("@/view/Comment.vue")
+}, {
+    path: "/calendar",
+    name: "Calendar",
+    component: () => import("@/view/Calendar.vue")
+}, {
+    path: "/setting",
+    name: "Setting",
+    component: () => import("@/view/Setting.vue")
 }]
