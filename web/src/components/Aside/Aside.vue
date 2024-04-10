59f7b130b5e52e25ab88cde02918a8d4890a3156
pkc918 <xiaopan2000918@gmail.com>
2024-04-10 23:28:07 +0800

    feat: added to link path by custom

--- web/src/components/Aside/Aside.vue
+++ web/src/components/Aside/Aside.vue
@@ -1,26 +1,59 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+
+interface routesItem{
+  iconName: string;
+  pathName: string;
+}
+const routesHashMapList: routesItem[] = [{
+  iconName: "acorn-duotone",
+  pathName:  "Chat"
+}, {
+  iconName: "android-logo-duotone",
+  pathName: "Friend"
+}, {
+  iconName: "codesandbox-logo-duotone",
+  pathName: "Collect"
+}, {
+  iconName: "aperture-duotone",
+  pathName: "Comment"
+}, {
+  iconName: "battery-charging-vertical-duotone",
+  pathName: "Battery"
+}, {
+  iconName: "cloud-duotone",
+  pathName: "Weather"
+}, {
+  iconName: "calendar-dots-duotone",
+  pathName: "Calendar"
+}, {
+  iconName: "dots-three-circle-duotone",
+  pathName: "Setting"
+}]
+
+const activePath = ref<string>("")
+const handleToRoute = (pathName: string) => {
+  activePath.value = pathName
+  router.push({name: pathName})
+}
+
+onMounted(() => {
+})
 </script>
 
 <template>
   <div class="w-16 bg-[#CFD6DA] flex flex-col items-center justify-between">
     <div class="w-12 pt-17 flex flex-col items-center gap-8">
-      <img class="w-12 h-12 rd-2" src="https://avatars.githubusercontent.com/u/58922004?v=4"  alt=""/>
-      <div class="w-8 h-8 i-ph-acorn-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-android-logo-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-codesandbox-logo-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-aperture-duotone hover:text-[#57bf6b]" />
+      <img class="w-12 h-12 rd-2" src="https://avatars.githubusercontent.com/u/58922004?v=4" alt="" />
+      <div class="w-8 h-8" :class="[`i-ph-${r.iconName}`, `${r.pathName === activePath ? 'text-[#57bf6b]' : ''}`]" :key="r.iconName" v-for="r in routesHashMapList.slice(0,4)" @click="handleToRoute(r.pathName)" />
     </div>
     <div class="w-12 pb-8 flex flex-col items-center gap-8">
-      <div class="w-8 h-8 i-ph-battery-charging-vertical-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-cloud-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-calendar-dots-duotone hover:text-[#57bf6b]" />
-      <div class="w-8 h-8 i-ph-aperture-duotone hover:text-[#57bf6b]" />
-      
+      <div class="w-8 h-8" :class="[`i-ph-${r.iconName}`, `${r.pathName === activePath ? 'text-[#57bf6b]' : ''}`]" :key="r.iconName" v-for="r in routesHashMapList.slice(4,routesHashMapList.length)" @click="handleToRoute(r.pathName)" />
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
