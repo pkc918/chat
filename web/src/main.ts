b4e7258a98d2fbf032b46e8335d6824d0cfdcda8
pkc918 <xiaopan2000918@gmail.com>
2024-04-11 09:20:26 +0800

    feat: added plugin pinia

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { pinia } from "@/store";
 import router from "@/router/router.ts";
+
 import "@/styles/index.css";
 import 'virtual:uno.css'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(pinia).mount("#app");
