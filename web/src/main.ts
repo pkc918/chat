7bf5cfca6601fd95538f84d15814b92974e26687
pkc918 <xiaopan2000918@gmail.com>
2024-04-16 22:54:32 +0800

    feat: added plugin antdv

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { pinia } from "@/store";
 import router from "@/router/router.ts";
+import Antd from 'ant-design-vue';
 
+import 'ant-design-vue/dist/reset.css';
 import "@/styles/index.css";
 import 'virtual:uno.css'
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(Antd).use(router).use(pinia).mount("#app");
