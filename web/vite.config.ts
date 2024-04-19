c87a69f01787cd77bf4fd9a9a7786c2838e05845
pkc918 <xiaopan2000918@gmail.com>
2024-04-19 09:07:57 +0800

    feat: added env vars

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,15 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Vue(), UnoCSS()],
+  server:{
+    proxy: {
+      "/api": {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
