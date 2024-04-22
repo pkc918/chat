51d5c3d07d5e7a39b8cbbab6cbe6753a00329914
pkc918 <xiaopan2000918@gmail.com>
2024-04-22 22:59:34 +0800

    feat: added basic page for chat with friend

--- web/src/components/Chat/ChatContent.vue
+++ web/src/components/Chat/ChatContent.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
 
+import { ref } from "vue";
+
+const message = ref<string>("")
 </script>
 
 <template>
-<div>聊天记录</div>
+<div class="w-full h-full flex flex-col justify-between">
+  <header class="w-full min-h-15 max-h-20 border-b-1 border-b-[#E0E0E0] border-b-solid flex justify-between items-center px-5">
+    <div>
+      <h3>陪我去看海吧！(165)</h3>
+    </div>
+    <div class="i-ph-dots-three-circle-duotone text-8"></div>
+  </header>
+  <main class="flex-1 p-5">
+    聊天内容
+  </main>
+  <div class="w-full h-65 flex flex-col border-t-1 border-t-[#E0E0E0] border-t-solid px-5">
+    <div class="w-full flex justify-between py-5 text-8">
+      <div class="flex gap-5">
+        <div class="i-ph-bug-beetle-duotone hover:text-[#57bf6b]"></div>
+        <div class="i-ph-tree-palm-duotone hover:text-[#57bf6b]"></div>
+        <div class="i-ph-twitch-logo-duotone hover:text-[#57bf6b]"></div>
+        <div class="i-ph-dots-three-circle-duotone hover:text-[#57bf6b]"></div>
+      </div>
+      <div class="flex gap-5">
+        <div class="i-ph-film-reel-duotone hover:text-[#57bf6b]"></div>
+        <div class="i-ph-phone-call-duotone hover:text-[#57bf6b]"></div>
+      </div>
+    </div>
+    <a-textarea class="w-full! h-full! p-0! text-4 appearance-none! shadow-none! border-none! resize-none! outline-none! bg-transparent" v-model:value="message" placeholder="Basic usage" :rows="4" />
+  </div>
+</div>
 </template>
 
 <style scoped>
--- web/src/view/Chat.vue
+++ web/src/view/Chat.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-
+import { ref } from "vue";
 import Layout from "@/components/Layout/Layout.vue";
 import UserItem from "@/components/Chat/UserItem.vue";
-import { ref } from "vue";
 import ChatContent from "@/components/Chat/ChatContent.vue";
 
 
